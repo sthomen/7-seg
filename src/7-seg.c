1fed5054a3b36b8eb91d7f2b3955d76b61ce2873
Staffan Thomén <duck@shangtai.net>
2014-12-10 20:51:19 +0200

    Shrink the digits and add a blinking colon

--- src/7-seg.c
+++ src/7-seg.c
@@ -4,9 +4,12 @@
 
 static Window *window_main;
 static Layer *time_layer;
+static Layer *colon_layer;
 
 static GBitmap *seg_hor;
 static GBitmap *seg_vert;
+
+static bool blink=false;
 
 struct tm *now=NULL;
 
@@ -14,7 +17,29 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
 	now=tick_time;
 
-	layer_mark_dirty(time_layer);
+	switch (units_changed) {
+		case MINUTE_UNIT:
+			layer_mark_dirty(time_layer);
+		case SECOND_UNIT:
+			layer_mark_dirty(colon_layer);
+			break;
+		default:
+			break;
+	}
+}
+
+static void update_colon_layer(Layer *this, GContext *ctx)
+{
+	graphics_context_set_fill_color(ctx, GColorClear);
+
+	if (blink) {
+		graphics_fill_rect(ctx, GRect(0,0,8,8), 0, (GCornerMask)NULL);
+		graphics_fill_rect(ctx, GRect(0,18,8,8), 0, (GCornerMask)NULL);
+
+		blink=false;
+	} else {
+		blink=true;
+	}
 }
 
 static void update_time_layer(Layer *this, GContext *ctx)
@@ -22,17 +47,17 @@ static void update_time_layer(Layer *this, GContext *ctx)
 	int d, i, j;
 	char *digit;
 
-	char time[5]="1234";
+	char time[5]="LoAd";
 
-	if (now==NULL)
-		return;
-
-	snprintf(time, 5, "%02d%02d", now->tm_hour, now->tm_min);
+	if (now!=NULL) {
+		snprintf(time, 5, "%02d%02d", (clock_is_24h_style() ? now->tm_hour : now->tm_hour % 12),
+			now->tm_min);
+	}
 
 	// these are relative to the graphics layer, vertical is always 0
-	int choffsets[4]={1,37,73,109};
+	int choffsets[4]={1,34,80,113};
 	// offsets relative to character
-	static int hoffsets[3]={0,4,29};
+	static int hoffsets[3]={0,4,25};
 	static int voffsets[9]={0,0,0,4,32,4,36,64,36};
 
 	for (d=0;d<4;d++) {			// digit
@@ -60,12 +85,16 @@ static void update_time_layer(Layer *this, GContext *ctx)
 
 static void window_main_load(Window *window)
 {
+	time_t t;
+
 	seg_hor	= gbitmap_create_with_resource(RESOURCE_ID_HORIZONTAL_SEGMENT);
 	seg_vert = gbitmap_create_with_resource(RESOURCE_ID_VERTICAL_SEGMENT);
 }
 
 static void window_main_unload(Window *window)
 {
+	layer_destroy(time_layer);
+	layer_destroy(colon_layer);
 }
 
 static void init()
@@ -84,10 +113,17 @@ static void init()
 
 	tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 
+	/* setup time layer */
 	time_layer = layer_create(GRect(0,50,144,69));
 
 	layer_add_child(window_get_root_layer(window_main), time_layer);
 	layer_set_update_proc(time_layer, update_time_layer);
+
+	/* setup colon layer */
+	colon_layer = layer_create(GRect(68,71,8,26));
+
+	layer_add_child(window_get_root_layer(window_main), colon_layer);
+	layer_set_update_proc(colon_layer, update_colon_layer);
 }
 
 static void deinit()
